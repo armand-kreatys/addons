4479896d015f9df1145ed1c227cbf43d05f77c04
armand-kreatys <armand@kreatys.com>
2025-01-28 22:01:58 +0100

    up

--- kreatys_price_custom/__manifest__.py
+++ kreatys_price_custom/__manifest__.py
@@ -2,11 +2,12 @@
     'name': 'Kreatys Price Custom',
     'version': '1.0',
     'summary': 'This module adds an action to update sale order line prices based on pricelist rules',
-    'author': 'Armand Gillot, Kreatys',
+    'author': 'kreatys-armand',
     'depends': ['sale'],
     'data': [
         'views/sale_order_views.xml',
     ],
+    'category': 'KREATYS',
     'installable': True,
     'application': True,
 }
