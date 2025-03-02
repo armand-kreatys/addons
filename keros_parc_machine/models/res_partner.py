421efe2b77d0c68e93c2c7ec468875866878c598
armand-kreatys <armand@kreatys.com>
2025-03-02 15:31:01 +0100

    update

--- keros_parc_machine/models/res_partner.py
+++ keros_parc_machine/models/res_partner.py
@@ -1,25 +1,24 @@
-from odoo import fields, models, _
 from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
-    machine_count = fields.Integer(string="Machines",
-                                   compute='compute_machine_count',
-                                   default=0)
+
+    machine_count = fields.Integer(
+        string="Machines", compute='compute_machine_count', default=0)
 
     def compute_machine_count(self):
         for record in self:
             record.machine_count = self.env['parc.machine'].search_count(
-                [('partner_id', '=', self.id)])
+                [('partner_id', '=', record.id)])
 
     def action_get_machines_record(self):
         self.ensure_one()
         return {
             'type': 'ir.actions.act_window',
             'name': 'Machines',
-            'view_mode': 'list',
+            'view_mode': 'list,form,kanban',
             'res_model': 'parc.machine',
             'domain': [('partner_id', '=', self.id)],
-            'context': "{'create': True}"
+            'context': {'default_partner_id': self.id, 'create': True}
         }
